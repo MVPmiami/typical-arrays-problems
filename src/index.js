c56c5dd6e4272f449b95fea747249e298eb3b601
Aliaksei Siankevich <andfly@tut.by>
2020-10-01 13:32:24 +0300

    feat: add 3 function for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length && array.length === 0){
+    return 0;
+  }else if(arguments.length && array.length !== 0){
+    array.sort(function(a,b) {
+    return b - a;
+  });
+    return array[array.length - 1];
+  }else{
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length && array.length === 0){
+    return 0;
+  }else if(arguments.length && array.length !== 0){
+    array.sort(function(a,b) {
+    return a - b;
+  });
+    return array[array.length - 1];
+  }else{
+    return 0;
+  }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length && array.length === 0){
+    return 0;
+  }else if(arguments.length && array.length !== 0){
+    let sum = 0;
+    array.forEach(function(item) {
+      sum += item;
+    });
+    return sum / array.length;
+  }else{
+    return 0;
+  }
 }
